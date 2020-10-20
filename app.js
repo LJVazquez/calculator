8cd148acdcecaafe8baa1c9402eb3f789d36dd8e
Ljvazquez <ljvazquez00@gmail.com>
2020-10-19 22:25:34 -0300

    started  refact

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ function divide(a, b){
     return +a * 1000 / +b * 1000 / 1000000; // POR LOS PROBLEMAS DE JS CON FLOATS
 }
 
+operandsDictionary = [{'SUM': '+'}, {'SUS': '-'}, {'PROD': '*'}, {'DIV': '/'}];
+
 function operate(numA, operand, numB){
     if(operand === '+'){
         result = add(numA, numB).toString();
@@ -30,15 +32,11 @@ function operate(numA, operand, numB){
     }
 }
 
-const numbers = document.querySelectorAll('[data-num]');
 const operations = document.querySelectorAll('[data-op]')
-const displayPrevious = document.getElementById('display-previous');
-const displayCurrent = document.getElementById('display-current');
-const resultBtn = document.querySelector('[data-result]');
-const acBtn = document.getElementById('ac');
-const delBtn = document.getElementById('del');
-const dotBtn = document.getElementById('dot');
-const signBtn = document.getElementById('sign');
+
+
+const dotBtn = document.querySelector('[data-id = "dot"]');
+const signBtn = document.querySelector('[data-id = "sign"]');
 
 let isOperand = false;
 
@@ -47,148 +45,172 @@ let secondValue = [];
 let operandValue = '';
 let result = '';
 
-
-function updatePreviousDisplay(){
-    displayPrevious.innerText = 
-    `${firstValue.join('')} ${operandValue} ${secondValue.join('')}`;
-    displayCurrent.innerText = result;
+// Actualiza el display con los ultimos valores
+function updateDisplay(){
+    const display1 = $('[data-display="previous"]');
+    const display2 = $('[data-display="current"]');
+    display1.text(`${firstValue.join('')} ${operandValue} ${secondValue.join('')}`);
+    display2.text(result);
 }
 
-numbers.forEach((element) => {
 
-    element.addEventListener('click',() =>{
+// DEL
+// si isOperand y no hay segundo valor, borrar el operando
+// si no hay segundo valor, borrar con pop.() del primer valor
+// else borrar del segundo valor
 
-        if (isOperand === false  && result === ''){
-            
-            firstValue.push(element.dataset.num);
-            updatePreviousDisplay();
+function deleteLast(){
+    
+    if (isOperand && secondValue.length === 0){
 
-        } else if (isOperand === true  && result === ''){
-            secondValue.push(element.dataset.num);
-            updatePreviousDisplay();
+        isOperand = false;
+        operandValue = '';
+    
+    } else if (secondValue.length === 0){
 
-        } else {
-            secondValue.push(element.dataset.num);
-            updatePreviousDisplay();
-        }
-    })
-})
+        firstValue.pop();
 
-operations.forEach((element) =>{
+    } else {
 
-    element.addEventListener('click', () => {
+        secondValue.pop();
+    }
 
+    updateDisplay()
+}
 
-        if(isOperand && secondValue.length !== 0){
-            operate(firstValue.join(''), operandValue, secondValue.join(''));
-            firstValue = String(result).split();
-            secondValue = [];
-            operandValue = element.dataset.op;
-            updatePreviousDisplay();
-        }
-
-        operandValue = element.dataset.op;
-        isOperand = true;
-        updatePreviousDisplay();
-    })
-})
-
-acBtn.addEventListener('click', () =>{
+// establece vacio en todas las variables
+function allClear(){
     firstValue = [];
     secondValue = []; 
     operandValue = ''; 
     result = ''; 
     isOperand = false; 
-    updatePreviousDisplay();
-})
+    updateDisplay();
+}
 
+// BOTON .
+// isThereAdot checkea si el parametro tiene un punto en algun lugar
+// si isThereAdot = true, vuelve antes de poder pushear un nuevo punto.
 
-delBtn.addEventListener('click', ()=>{
-
-    if (secondValue.length === 0 && isOperand === true){
-
-        isOperand = false;
-        operandValue = '';
-        updatePreviousDisplay();
-    
-    } else if (secondValue.length === 0){
-
-        firstValue.pop();
-        updatePreviousDisplay()
-
-    } else {
-
-        secondValue.pop();
-        updatePreviousDisplay()
+function addDot(){
+    function isThereAdot(num){
+        for (let i = 0; i < num.length; i++){
+            if (num[i] === '.') return true;
+        }
     }
-})
 
+    if (!isOperand){
+        if (isThereAdot(firstValue)) return;
+        firstValue.push('.');
+    }
+    
+    else {
+        if (isThereAdot(secondValue)) return;
+        secondValue.push('.');
+    }
+    updateDisplay();
+}
 
-resultBtn.addEventListener('click', () => {
+// BOTON +/-
+// si isOperand entonces estamos en el primer valor, else estamos en el segundo
+// si isItNegative entonces el valor es negativo, entonces hay que shift el '-'
+// si !isItNegative entonces el valor es positivo, por lo que hay que unshift un '-'
+
+function changeSign(){
+
+    function isItNegative(num){
+        if (num[0] === '-') return true;
+    }
+
+    if (!isOperand){
+        isItNegative(firstValue) ? firstValue.shift() : firstValue.unshift('-');
+    }
+
+    else {
+        isItNegative(secondValue) ? secondValue.shift() : secondValue.unshift('-');
+    }
+
+    updateDisplay();
+}
+
+// BOTON RESULT
+// si result no esta vacio quiere decir que ya hicimos una operacion, y si se vuelve
+// a tocar el boton hay que cambiar el primer valor a su resultado y volver a
+// realizar la operacion en operate() con el mismo segundo valor;
+function getResult(){
 
     if (result !== ''){
 
         firstValue = String(result).split();
         operate(firstValue, operandValue, secondValue.join(''));
-        updatePreviousDisplay();
-
-
+        updateDisplay();
     }
-
-
     operate(firstValue.join(''), operandValue, secondValue.join(''));
-    updatePreviousDisplay();
+    updateDisplay();
+}
 
 
-})
+// BOTONES DE NUMEROS
+// si firstValue, secondValue y result no esta vacios quiere decir que se hizo 
+// una cuenta asi que allClear y empezar una cuenta nueva
+// si isOperand entonces push el numero a secondValue
+// sino push el nro a firstValue
 
-
-// checkea en cada caracter de la string de displayCurrent por '.'
-// si lo encuentra retorna, de no encontrarlo lo agrega a displayCurrent.innerText;
-dotBtn.addEventListener('click', () =>{
-
-    if (isOperand === false){
-        for (let i = 0; i < firstValue.length; i++){
-
-            if (firstValue[i] === '.'){
+function addNumber(){
     
-                return;
-            } 
-    
-        } firstValue.push('.');
-    }
-    
-    else {
-        for (let i = 0; i < secondValue.length; i++){
+    $('[data-num]').each((index, element) =>{
 
-            if (secondValue[i] === '.'){
-    
-                return;
-            } 
-    
-        } secondValue.push('.');
-    }
+        $(element).click(() =>{
 
-    updatePreviousDisplay();
-})
+            if(firstValue.length !== 0 && secondValue.length !== 0 && result !== ''){
+                allClear();
+                firstValue.push(element.dataset.num);
+            }
 
-signBtn.addEventListener('click', () =>{
+            else if (isOperand) secondValue.push(element.dataset.num);
 
-    if (isOperand === false && firstValue[0] === '-'){
-        firstValue.shift();
-    }
+            else firstValue.push(element.dataset.num);
+            
+            updateDisplay();
+        })
+    })
+}
 
-    else if (isOperand === false){
-        firstValue.unshift('-');
-    }
 
-    else if (isOperand === true && secondValue[0] === '-'){
-        secondValue.shift();
-    }
+// si isOperand y segundo valor no esta vacio quiere decir que se terminó de hacer
+// una cuenta, asi que al clickear un operand establecer todo para hacer otra
+// cuenta con el resultado como nuevo primer parametro
+// si no establecer el operand y asignar isOperand para saber que pasamos al
+// segundo valor al presionar algun numero
 
-    else{
-        secondValue.unshift('-')
-    }
+function addOperator(){
 
-    updatePreviousDisplay();
-})
+    $('[data-op]').each( (index, element) =>{
+
+        $(element).click(() =>{
+
+            if(isOperand && secondValue.length !== 0){
+                operate(firstValue.join(''), operandValue, secondValue.join(''));
+                firstValue = String(result).split();
+                secondValue = [];
+            }
+
+            operandValue = element.dataset.op;
+            isOperand = true;
+            updateDisplay();
+            result = '';
+        })
+    })
+}
+
+function startApp(){
+    $('[data-id="del"]').click(deleteLast);
+    $('[data-id="ac"]').click(allClear);
+    $('[data-id="sign"]').click(changeSign);
+    $('[data-id="dot"]').click(addDot);
+    $('[data-id="result"]').click(getResult);
+    addNumber();
+    addOperator();
+}
+
+startApp();
